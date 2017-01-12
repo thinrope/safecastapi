4505fa5e1a3aec4eeb22b7c1e811841f08ccbc74
Eito Katagiri <eitoball@gmail.com>
2017-01-12 11:37:54 +0900

    disable to use compact style for now

--- app/controllers/v1/measurements_controller.rb
+++ app/controllers/v1/measurements_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class V1::MeasurementsController < ActionController::Base
+class V1::MeasurementsController < ActionController::Base # rubocop:disable Style/ClassAndModuleChildren
   def create
     render nothing: true, status: 202
   end
